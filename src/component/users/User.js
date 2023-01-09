19dc62ec49d9910d38a8657b8a48b84313558703
AlexAntoine <anotoinealex1234@gmail.com>
2023-01-08 20:17:43 -0500

    added more components for user page

--- src/component/users/User.js
+++ src/component/users/User.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-
+import React, { Fragment, Component } from 'react'
+import Spinner from '../Layout/spinner';
+import {Link} from 'react-router-dom'
 export class User extends Component {
 
 componentDidMount(){
@@ -13,6 +14,7 @@ componentDidMount(){
       name,
       avatar_url,
       location,
+      company,
       bio,
       blog,
       login,
@@ -25,10 +27,57 @@ componentDidMount(){
     } = this.props.user
 
     const {loading} = this.props;
+
+    if(loading){
+      return <Spinner/>
+    }
     return (
-      <div>{name}</div>
+      <Fragment>
+        <Link to='/' className='btn btn-light'>Back To Search </Link>
+        Hireable:{}
+        {hireable ? <i className="fas fa-check text-success"/> : <i className="fas fa-times-circle text-danger"/>}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img src={avatar_url} className="round-img" alt="" style={{width:'150px'}}/>
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+
+          <div>
+            {bio && <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+            }
+            <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
+            <ul>
+              <li>
+                {login && <Fragment>
+                <strong>Username: </strong>{login}
+              </Fragment>}
+              </li>
+              <li>
+                {company && <Fragment>
+                <strong>Company: </strong>{company}
+              </Fragment>}
+              </li>
+              <li>
+                {blog && <Fragment>
+                <strong>Website: </strong>{blog}
+              </Fragment>}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-sucess">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public GIsts: {public_gists}</div>
+        </div>
+      </Fragment>
     )
   }
 }
-
 export default User
